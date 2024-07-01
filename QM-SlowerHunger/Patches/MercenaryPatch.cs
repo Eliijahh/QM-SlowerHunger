4f1bfcd56efd901e856c1ea2d0c9bbefd23db379
Paolo Caponeri <paolo.caponeri@gmail.com>
2024-07-02 01:09:57 +0200

    Initial setup
    Need to fix the code, currently not working as intended. Bigger problem is that to fix the round to int problem now it is not working anymore

---
+++ QM-SlowerHunger/Patches/MercenaryPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SlowerHunger.Patches
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(Mercenary), ("GetStarvMult"))]
+        [HarmonyPostfix]
+        static void decreaseHungerMultiplier(ref float __result)
+        {
+            __result = __result * 0.01f;
+        }
+
+        [HarmonyPatch(typeof(StarvationEffect), ("ProcessActionPoint"))]
+        [HarmonyPostfix]
+        static void transformCurrentLevelToFloat(StarvationEffect __instance, ref float num)
+        {
+            Traverse.Create(__instance).Property("CurrentLevel").SetValue(((float)__instance.Delta + num) * __instance.Mult);
+        }
+    }
+}
---
+++ QM-SlowerHunger/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SlowerHunger.Patches;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowerHunger
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SlowerHunger : BaseUnityPlugin
+    {
+        private const string modGUID = "Eliijahh.SlowerHunger";
+        private const string modName = "Slower Hunger";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SlowerHunger Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The Slower Hunger mod has awakened.");
+
+            harmony.PatchAll();
+        }
+    }
+}
